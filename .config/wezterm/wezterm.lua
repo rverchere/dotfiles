7027eb481266211cee97ac9239b157613d8684bc
Rémi Verchère <remi@verchere.fr>
2024-01-09 09:23:50 +0100

    chore: update wezterm
    Signed-off-by: Rémi Verchère <remi@verchere.fr>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,5 +14,14 @@ end
 config.color_scheme = 'nord'
 config.font = wezterm.font 'FiraCode Nerd Font'
 
+-- Disable fancy tab style
+config.use_fancy_tab_bar = false
+
+-- Hide the tab bar if there is only one tab
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Set Background opacity
+config.window_background_opacity = 0.99
+
 -- and finally, return the configuration to wezterm
 return config
